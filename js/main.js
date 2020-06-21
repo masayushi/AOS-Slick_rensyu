702bd274f47706fa30026f66c3e78d1d5debd12c
masayushi <4a1e0010@stust.edu.tw>
2020-06-21 16:33:50 +0800

    底下連結設定、加入文字滑入動畫、其他細部修正

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-$("#banner").slick();
-$("#brand").slick();
+AOS.init();
+
+$("#banner").slick({
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+});
+$("#brand").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    arrows: false,
+    variableWidth: true,
+});
